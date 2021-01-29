224a6af5e8faee3804ab675511792a84d338faa2
joel <joel@joelui-MacBookPro.local>
2021-01-30 04:15:36 +0900

    relative path

--- js/script.js
+++ js/script.js
@@ -84,9 +84,9 @@
     /*----------------------------section04: lunch, dinner, group----------------------------*/
 
     var section04Data =[
-        {name: 'lunch', img: "/img/main/lunch.jpg", time: '12:00 ~ 03:00', color: '#C4A484', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis suscipit eos aut quam ea consequatur quis error repudiandae et illo?'},
-        {name: 'dinner', img: "/img/main/dinner.jpg", time: '18:00 ~ 01:00', color: '#a4b8ba', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis suscipit eos aut quam ea.'},
-        {name: 'group', img: "/img/main/group.jpg", time: 'TABLE FOR 6 ~ 12', color: '#C4A484', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis suscipit eos aut quam ea consequatur quis?'}
+        {name: 'lunch', img: "./img/main/lunch.jpg", time: '12:00 ~ 03:00', color: '#C4A484', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis suscipit eos aut quam ea consequatur quis error repudiandae et illo?'},
+        {name: 'dinner', img: "./img/main/dinner.jpg", time: '18:00 ~ 01:00', color: '#a4b8ba', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis suscipit eos aut quam ea.'},
+        {name: 'group', img: "./img/main/group.jpg", time: 'TABLE FOR 6 ~ 12', color: '#C4A484', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis suscipit eos aut quam ea consequatur quis?'}
     ];
     var section04Back = $('#section04 .background-box');
     var section04Box = $('#section04 .view-box');
@@ -120,38 +120,38 @@
         {   
             category: 'starter',
             menus: [
-                {id: 0, name: 'lemon prawn vol-au-vents', cost: '$10.00', img:'/img/main/starter01.jpg', desc:'Fruity, tangy, crunchy and colourful, this easy fresh vegan salad makes for a fantastic light and healthy starter.'},
-                {id: 1, name: 'bruschetta', cost: '$9.00', img:'/img/main/starter02.jpg', desc:'Bruschetta is an Italian appetizer/starter dish consisting of grilled/baked bread that is brushed with garlic and olive oil'},
-                {id: 2, name: 'Great British Menu Starter', cost: '$11.00', img:'/img/main/starter03.jpg', desc:'This dish is simplistic elegance. No surprise ingredients or molecular cooking techniques. Just a superb array of the freshest ingredients we have today.'},
+                {id: 0, name: 'lemon prawn vol-au-vents', cost: '$10.00', img:'./img/main/starter01.jpg', desc:'Fruity, tangy, crunchy and colourful, this easy fresh vegan salad makes for a fantastic light and healthy starter.'},
+                {id: 1, name: 'bruschetta', cost: '$9.00', img:'./img/main/starter02.jpg', desc:'Bruschetta is an Italian appetizer/starter dish consisting of grilled/baked bread that is brushed with garlic and olive oil'},
+                {id: 2, name: 'Great British Menu Starter', cost: '$11.00', img:'./img/main/starter03.jpg', desc:'This dish is simplistic elegance. No surprise ingredients or molecular cooking techniques. Just a superb array of the freshest ingredients we have today.'},
             ]
         },
         {   
             category: 'salad',
             menus: [
-                {id: 0, name: 'chickpea salad', cost: '$13.00', img:'/img/main/salad01.jpg', desc:'If you like shawarma, you should definitely try this vegan chickpea shawarma salad by Deryn from Running on Real Food.'},
-                {id: 1, name: 'blt salad', cost: '$12.00', img:'/img/main/salad02.jpg', desc:'Melissa used homemade croutons and a simple yogurt based ranch dressing for this healthy and delicious vegan salad.'},
-                {id: 2, name: 'crunchy salad', cost: '$12.00', img:'/img/main/salad03.jpg', desc:'This crunchy broccoli salad is super healthy and completely oil-free. It’s perfect for a picnic or potluck.'},
-                {id: 3, name: 'farro salad', cost: '$12.00', img:'/img/main/salad04.jpg', desc:'super fresh and delicious. If you can find farro,  you could also replace it with quinoa, millet, or brown rice.'},
-                {id: 4, name: 'detox salad', cost: '$14.00', img:'/img/main/salad05.jpg', desc:'This detox crunch salad is loaded with healthy and fresh ingredients! It makes a delicious and healthy lunch or dinner.'},
+                {id: 0, name: 'chickpea salad', cost: '$13.00', img:'./img/main/salad01.jpg', desc:'If you like shawarma, you should definitely try this vegan chickpea shawarma salad by Deryn from Running on Real Food.'},
+                {id: 1, name: 'blt salad', cost: '$12.00', img:'./img/main/salad02.jpg', desc:'Melissa used homemade croutons and a simple yogurt based ranch dressing for this healthy and delicious vegan salad.'},
+                {id: 2, name: 'crunchy salad', cost: '$12.00', img:'./img/main/salad03.jpg', desc:'This crunchy broccoli salad is super healthy and completely oil-free. It’s perfect for a picnic or potluck.'},
+                {id: 3, name: 'farro salad', cost: '$12.00', img:'./img/main/salad04.jpg', desc:'super fresh and delicious. If you can find farro,  you could also replace it with quinoa, millet, or brown rice.'},
+                {id: 4, name: 'detox salad', cost: '$14.00', img:'./img/main/salad05.jpg', desc:'This detox crunch salad is loaded with healthy and fresh ingredients! It makes a delicious and healthy lunch or dinner.'},
             ]
         },
         {
             category: 'meal',
             menus: [
-                {id: 0, name: 'spaghetti carbonara', cost: '$17.00', img:'/img/main/pasta01.jpg', desc:'Discover how to make superb spaghetti carbonara. This cheesy pasta dish is an Italian favourite and with the right technique, you can make it perfect every time.'},
-                {id: 1, name: 'Fettuccine alfredo', cost: '$16.00', img:'/img/main/pasta02.jpg', desc:' This sumptuous Italian dish is for you.'},
-                {id: 2, name: 'Spaghetti puttanesca', cost: '$16.00', img:'/img/main/pasta03.jpg', desc:'true Italian classic with a meaty, chilli sauce. Serve straight from the dish with a dressed green salad.'},
-                {id: 3, name: 'Tortilla pizza', cost: '$20.00', img:'/img/main/pizza01.jpg', desc:'homemade toppings on this easy tortilla pizza, with plenty of melted mozzarella.'},
-                {id: 4, name: 'Margherita pizza', cost: '$21.00', img:'/img/main/pizza02.jpg', desc:"a homemade Margherita pizza topped with fresh tomato sauce and melted cheese. Here's how to master this everyday classic."},
+                {id: 0, name: 'spaghetti carbonara', cost: '$17.00', img:'./img/main/pasta01.jpg', desc:'Discover how to make superb spaghetti carbonara. This cheesy pasta dish is an Italian favourite and with the right technique, you can make it perfect every time.'},
+                {id: 1, name: 'Fettuccine alfredo', cost: '$16.00', img:'./img/main/pasta02.jpg', desc:' This sumptuous Italian dish is for you.'},
+                {id: 2, name: 'Spaghetti puttanesca', cost: '$16.00', img:'./img/main/pasta03.jpg', desc:'true Italian classic with a meaty, chilli sauce. Serve straight from the dish with a dressed green salad.'},
+                {id: 3, name: 'Tortilla pizza', cost: '$20.00', img:'./img/main/pizza01.jpg', desc:'homemade toppings on this easy tortilla pizza, with plenty of melted mozzarella.'},
+                {id: 4, name: 'Margherita pizza', cost: '$21.00', img:'./img/main/pizza02.jpg', desc:"a homemade Margherita pizza topped with fresh tomato sauce and melted cheese. Here's how to master this everyday classic."},
             ]
         },
         {
             category: 'dessert',
             menus: [
-                {id: 0, name: 'raspberry cake', cost: '$23.00', img:'/img/main/dessert01.jpg', desc:'Like a Victoria sponge but better, try budget-friendly frozen raspberries for the cream'},
-                {id: 1, name: 'crêpes', cost: '$17.00', img:'/img/main/dessert02.jpg', desc:' crêpe cake is perfect for a special occasion. We love the combination of sweet pancakes and a sharp, creamy filling.'},
-                {id: 2, name: 'chocolate fudge', cost: '$12.00', img:'/img/main/dessert03.jpg', desc:"the chocolate cake with an airy, light sponge and rich buttercream filling. It's simple enough for an afternoon tea but special enough for a party too."},
-                {id: 3, name: 'Belgian waffles', cost: '$13.00', img:'/img/main/dessert04.jpg', desc:'These delicious vegan pancakes can be sweet or savoury and are super adaptable to every taste. A great low-calorie breakfast or brunch option.'},
+                {id: 0, name: 'raspberry cake', cost: '$23.00', img:'./img/main/dessert01.jpg', desc:'Like a Victoria sponge but better, try budget-friendly frozen raspberries for the cream'},
+                {id: 1, name: 'crêpes', cost: '$17.00', img:'./img/main/dessert02.jpg', desc:' crêpe cake is perfect for a special occasion. We love the combination of sweet pancakes and a sharp, creamy filling.'},
+                {id: 2, name: 'chocolate fudge', cost: '$12.00', img:'./img/main/dessert03.jpg', desc:"the chocolate cake with an airy, light sponge and rich buttercream filling. It's simple enough for an afternoon tea but special enough for a party too."},
+                {id: 3, name: 'Belgian waffles', cost: '$13.00', img:'./img/main/dessert04.jpg', desc:'These delicious vegan pancakes can be sweet or savoury and are super adaptable to every taste. A great low-calorie breakfast or brunch option.'},
             ]
         }
     ];
